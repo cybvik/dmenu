36d58707720aa6efe69240a75ff78f883e7ce8f8
cybvik <cybvik@cybvik.xyz>
2022-06-07 12:19:13 +0200

    Change colors

--- config.def.h
+++ config.def.h
@@ -9,16 +9,16 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#f8f8f2", "#282a36" },
-	[SchemeSel] = { "#f8f8f2", "#6272a4" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	/*                        fg         bg       */
+	[SchemeNorm] = {          "#bbbbbb", "#222222" },
+	[SchemeSel] = {           "#eeeeee", "#005577" },
+	[SchemeSelHighlight] = {  "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeOut] = {           "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 0;
-static unsigned int columns    = 0;
+static unsigned int lines      = 10;
+static unsigned int columns    = 2;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -27,4 +27,4 @@ static unsigned int columns    = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 2;
