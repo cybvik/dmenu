d005865885c5daf54a855807b86ba5a991f45605
cybvik <cybvik@cybvik.xyz>
2022-06-25 01:29:50 +0200

    Change colors

--- config.def.h
+++ config.def.h
@@ -10,10 +10,10 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                        fg         bg       */
-	[SchemeNorm] = {          "#bbbbbb", "#222222" },
-	[SchemeSel] = {           "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = {  "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = {          "#ea00d9", "#091833" },
+	[SchemeSel] = {           "#091833", "#0abdc6" },
+	[SchemeSelHighlight] = {  "#bbbbbb", "#0000ff" },
+	[SchemeNormHighlight] = { "#ffff00", "#000000" },
 	[SchemeOut] = {           "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
